51b47123c80c9701b17b4f01c4e8f41ca34f192d
sabarikennady <sabareeshwarankennady@gmail.com>
2021-06-11 11:46:15 +0530

    Completed all basics

---
+++ src/components/Library.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LibrarySong from "./LibrarySongs";
+
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
+      <h2>Library</h2>
+      <div className="library-sonngs">
+        {songs.map((song) => (
+          <LibrarySong
+            songs={songs}
+            song={song}
+            setCurrentSong={setCurrentSong}
+            id={song.id}
+            key={song.id}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
---
+++ src/data.js
@@ -0,0 +1,102 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chilliHop() {
+  return [
+    {
+      name: "Yaakkai Thiri",
+      cover:
+        "https://c-sf.smule.com/rs-s91/arr/b3/0d/71f520d3-95f8-4e2f-9a09-ccd16a5fda53_1024.jpg",
+      artist:
+        "Music: A. R. Rahman. Singers: A.R. Rahman, Sunitha Sarathy & Shalini Singh",
+      audio:
+        "https://www.tamilmp3plus.com/songs/mp3/aar/Ayudha%20Ezhuthu%20(2004)%20-%20Fanna.mp3",
+      color: ["#87a9db", "#b36759"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Salomia",
+      cover: "https://i.ytimg.com/vi/7z20SuH4n2U/maxresdefault.jpg",
+      artist: "Deva",
+      audio: "https://www.tamilmp3plus.com/songs/mp3/90/Salomiya.m4a",
+      color: ["#b9bc6f", "#eda162"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "ooh la la la",
+      cover: "https://i.ytimg.com/vi/WfNaT4v0Psc/hqdefault.jpg",
+      artist: "Unni Menon, K. S. Chitra, Srinivas",
+      audio:
+        "https://www.tamilmp3plus.com/songs/mp3/90/Huu%20Huu%20Lalala-Manamadurai.m4a",
+      color: ["#d96b43", "#ed87cd"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Malargale Malargale",
+      cover:
+        "https://www.filmibeat.com/img/popcorn/movie_posters/love-birds-20170403161754-6310.jpg",
+      artist: "Music: A. R. Rahman. Singers: K. S. Chithra, Hariharan.",
+      audio: "https://www.tamilmp3plus.com/songs/mp3/90/Malargalae.m4a",
+      color: ["#7ab5ba", "#ebe066"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Poongatrilae",
+      cover:
+        "https://d3tstbtkvp70pc.cloudfront.net/sites/default/files/uyire.jpg",
+      artist: "Music: A. R. Rahman. Singers : Unni Menon, Swarnalatha",
+      audio: "https://www.tamilmp3plus.com/songs/mp3/90/Poongatrile.m4a",
+      color: ["#cb7d46", "#000000"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mellisayae",
+      cover:
+        "https://www.filmibeat.com/img/popcorn/movie_posters/mr-romeo-20170403170153-15922.jpg",
+      artist: "Music: A. R. Rahman. Singers: K. S. Chithra, Hariharan.",
+      audio: "https://www.tamilmp3plus.com/songs/mp3/90/Mellisaiyae.m4a",
+      color: ["#872a51", "#FFD700"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Poo Pookum Osai",
+      cover:
+        "https://lyricsraag.com/wp-content/uploads/2020/10/poo-pookum-osai-lyrics-english-translation.jpg",
+      artist: "Music: A. R. Rahman. Singers: Sujatha, Malaysia Vasudevan",
+      audio:
+        "https://www.tamilmp3plus.com/songs/mp3/aar/Minsaara%20Kanavu%20(1997)%20-%20PooPookkum.mp3",
+      color: ["#4685f2", "#4ef542"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Indirayo Ival Sundariyo",
+      cover:
+        "https://www.tamil2lyrics.com/wp-content/uploads/2017/10/indhiraiyo.png",
+      artist: "Music: A. R. Rahman. Singers: Sunanda Minmini",
+      audio:
+        "https://www.tamilmp3plus.com/songs/mp3/aar/Kaadhalan%20(1994)%20-%20IndiraiyoIval.mp3",
+      color: ["#f2f246", "#4ebf19"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Kappaleri Poyachu",
+      cover:
+        "https://www.nowrunning.com/content/movie/1996/india-7972/bg-video413202025953.jpg",
+      artist: "Music: A. R. Rahman. Singers:  SPB, P Susheela",
+      audio:
+        "https://www.tamilmp3plus.com/songs/mp3/aar/Indian%20(1996)%20-%20KappaleriPoyaachu.mp3",
+      color: ["#bf7219", "#4ef542"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chilliHop;
